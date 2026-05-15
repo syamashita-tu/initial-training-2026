82e2d540135d9a145ce35192e4c63e019098adf0
syamashita-tu <syamashita@SurfacePro8.localdomain>
2026-05-15 17:01:26 +0900

    260515

--- syamashita-tu/mkfl.py
+++ syamashita-tu/mkfl.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
-folder = Path("syamashita-tu/chapter04")
+folder = Path("syamashita-tu/chapter05")
 n_list = list(range(1, 11))  # 任意の数を指定
 
 folder.mkdir(parents=True, exist_ok=True)
 
 for n in n_list:
-    file_path = folder / f"q0{n}.ipynb"
+    if n < 10:
+        file_path = folder / f"q0{n}.ipynb"
+    else:
+        file_path = folder / f"q{n}.ipynb"
     file_path.touch(exist_ok=True)
