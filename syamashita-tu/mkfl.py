60115041db8a11c5e0c1ea0ae0cabff0a6cf1ea3
syamashita-tu <gui2526tar@gmail.com>
2026-07-10 00:11:29 +0900

    260710

--- syamashita-tu/mkfl.py
+++ syamashita-tu/mkfl.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
-folder = Path("syamashita-tu/chapter10")
-n_list = list(range(1, 7))  # 任意の数を指定
+folder = Path("syamashita-tu/chapter12")
+n_list = list(range(1, 12))  # 任意の数を指定
 
 folder.mkdir(parents=True, exist_ok=True)
 
---
+++ syamashita-tu/vad/src/vad.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+import numpy as np
+import soundfile as sf
+from IPython.display import Audio, display
+
